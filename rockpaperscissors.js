917b4ea4e3c89ee6e1384b547174f94a19fe2e02
gvianna <gjosevianna@gmail.com>
2020-08-20 23:16:07 -0300

    Implemented conditional results

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -32,26 +32,27 @@ function runButton(){
       battlesection.id = 'battlesection';
       battlediv.appendChild(battlesection);
       document.querySelector('input#runbtn').disabled = true;
-      //battlesection.innerHTML = ` ${plassign} VS {PLACEHOLDER}! `;
         let imgtags = document.getElementsByTagName('img');
           for (let i=0; i < imgtags.length; i++){
             imgtags[i].onclick = false;
             //imgtags[i].onclick = choiceVerif();
           }
-        let foebattle = document.createElement('img')
+        let foebattle = document.createElement('img');
         let foechoice = [rock, paper, scissor];
         let foeresult = foechoice[Math.floor(Math.random() * foechoice.length)];
-        foeassign = foeresult;
-        if (foeassign === rock){
+        if (foeresult == rock){
+          foeassign = 'Rock';
           foebattle.setAttribute('src', 'img/rock.png');
-        } else if (foeassign === paper){
+        } else if (foeresult == paper){
+          foeassign = 'Paper';
           foebattle.setAttribute('src', 'img/paper.png');
-        } else if (foeassign === scissor){
+        } else if (foeresult == scissor){
+          foeassign = 'Scissor';
           foebattle.setAttribute('src', 'img/scissor.png');
         }
-        document.getElementById('battlesection').appendChild(foebattle);
+      document.getElementById('battlesection').appendChild(foebattle);
+      foebattle.id += 'foebattle';
         let plbattle = document.createElement('img');
-          foebattle.id += 'foebattle';
         if (plassign == 'Rock'){
           plbattle.setAttribute('src', 'img/rock.png');
         } else if (plassign == 'Paper'){
@@ -61,10 +62,32 @@ function runButton(){
         }
       document.getElementById('battlesection').appendChild(plbattle);
       plbattle.id += 'plbattle';
-      let vsbattle = document.createElement('img');
-      vsbattle.setAttribute('src', 'img/vs.png');
+        let vsbattle = document.createElement('img');
+        vsbattle.setAttribute('src', 'img/vs.png');
       document.getElementById('battlesection').appendChild(vsbattle);
       vsbattle.id += 'vsbattle';
+      console.log(` ${plassign} vs ${foeassign} `);
+      if (plassign == 'Rock' && foeassign == 'Rock'){
+        console.log('Its a tie!');
+      } else if (plassign == 'Rock' && foeassign == 'Paper'){
+        console.log('You lost!');
+      } else if (plassign == 'Rock' && foeassign == 'Scissor'){
+        console.log('You won!');
+      } else if (plassign == 'Paper' && foeassign == 'Rock'){
+        console.log('You won!');
+      } else if (plassign == 'Paper' && foeassign == 'Paper'){
+        console.log('Its a tie!');
+      } else if (plassign == 'Paper' && foeassign == 'Scissor'){
+        console.log('You lost!');
+      } else if (plassign == 'Scissor' && foeassign == 'Rock'){
+        console.log('You lost!');
+      } else if (plassign == 'Scissor' && foeassign == 'Paper'){
+        console.log('You won!');
+      } else if (plassign == 'Scissor' && foeassign == 'Scissor'){
+        console.log('Its a tie!');
+      }
+
     }
+
   }
 }
